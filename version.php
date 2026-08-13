ee1ae342ff662d8307df1166232ef5e02c9ae04e
Eduardo Kraus <kraus@eduardokraus.com>
2026-08-13 12:48:14 -0300

    V 1.9.6

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2026081300;
-$plugin->release = '1.9.5';
+$plugin->release = '1.9.6';
 $plugin->component = 'local_kopere_recert';
 $plugin->requires = 2024100700;
 $plugin->maturity = MATURITY_STABLE;
